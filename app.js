52ff21e9a33e1481fb981fd40f3376ab7d407cdd
Gaurav Kumar <66291225+gaurav645@users.noreply.github.com>
2023-03-17 23:21:51 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { runNmapWorker } = require('./nmapWorker');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.json());
 
